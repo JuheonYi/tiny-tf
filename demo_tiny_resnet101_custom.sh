2f0ffbba819cd9dde17dcaab7bbcf5a28a2bc08a
JuheonYi <johnyi0606@gmail.com>
2019-04-25 01:02:47 +0900

    added eval_tool codes & examples results

--- demo_tiny_resnet101_custom.sh
+++ demo_tiny_resnet101_custom.sh
@@ -2,7 +2,7 @@
 
 python3 ./demo_net.py \
 --gpu 0 \
---weights ./output/Resnet101_04-23-14-14/ \
+--weights ./output/Resnet101/ \
 --cfg ./cfgs/tiny_resnet101.yml \
 --refBox ./data/RefBox_N25_scaled.mat \
 --network Resnet101_test
--- tiny_resnet101_eval.sh
+++ tiny_resnet101_eval.sh
@@ -2,7 +2,7 @@
 
 python3 ./test_net.py \
 --gpu 0 \
---weights ./output/Resnet101_tiny/ \
+--weights ./output/Resnet101/ \
 --cfg ./cfgs/tiny_resnet101.yml \
 --refBox ./data/RefBox_N25_scaled.mat \
 --pkl_file ./data/pickles/wider_val_detail.pkl \
